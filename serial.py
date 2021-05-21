651c12eaaa9a7c37e664934a246e260f52ba8c5a
colin <colin@example.com>
2021-05-21 14:40:21 -0700

    baby step giant step implemented

--- serial.py
+++ serial.py
@@ -1,7 +1,8 @@
-
+import math
 # Find smallest generator g for given prime number
 def serial_find_g(p):
     check = list(range(1,p))
+    all_vals = []
     for i in range(2,p):
         m = 0
         sol = [0]*len(check)
@@ -12,7 +13,29 @@ def serial_find_g(p):
 
         sol.sort()
         if sol == check:
-            return i   
+            all_vals.append(i)
+            
+
+    return all_vals   
 
 # Solve Baby-Step, Giant-Step algorithm with given generator g, remainder h, and prime p
 def serial_bs_gs(g, h, p):
+    n = math.ceil(math.sqrt(p-1))
+    bs = {}
+    for i in range(n):
+        #bs[g**(i)%p] = i
+        bs[pow(g,i,p)] = i
+
+    #temp = g**(n*(p-2)) % p
+    temp = pow(g, n*(p-2), p)
+    for j in range(n):
+        #gs = (h * temp**j % p) % p
+        gs = h * pow(temp,j,p) % p
+        if gs in bs:
+            return j * n + bs[gs]
+
+    return None
+
+
+
+
